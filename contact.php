cc9bebd861e600a2febeb1909e4cf361cb0edad9
shrall <54165490+shrall@users.noreply.github.com>
2020-03-26 21:26:19 +0700

    done

--- contact.php
+++ contact.php
@@ -2,17 +2,16 @@
 if(isset($_POST['submit'])){
     $to = "shrallvierdo@gmail.com";
     $from = $_POST['email'];
-    $full_name = $_POST['first_name'];
-    $subject = "Form submission";
+    $full_name = $_POST['full_name'];
+    $subject = "shrall.xyz contact form submission";
     $subject2 = "Copy of your form submission";
     $message = $full_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
-
+    $message2 = "Here is a copy of your message " . $full_name . "\n\n" . $_POST['message'];
+    $result ='<div id="alert">Message has been sent! Thank you! I will contact you shortly.</div>';
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
     mail($from,$subject2,$message2,$headers2); 
-    echo "Mail Sent. Thank you " . $full_name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    header('Location: thankyou.php'); 
     }
 ?>
